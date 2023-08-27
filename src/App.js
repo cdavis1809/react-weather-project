f052a7822b3280856b83d7aa3525109e56f924ad
cdavis1809 <133775062+cdavis1809@users.noreply.github.com>
2023-08-27 08:53:37 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h2>Weather App</h2>
+      <div className="container">
+        <h2>Weather App</h2>
+        <footer>
+          This project was coded by Courtney Davis and is{" "}
+          <a
+            href="https://github.com/cdavis1809?tab=repositories"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
