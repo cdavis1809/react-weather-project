f78b9e1b554daa8658233357426ac307a8c01cc2
cdavis1809 <133775062+cdavis1809@users.noreply.github.com>
2023-09-21 07:31:06 -0400

    updated function

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -25,6 +25,8 @@ export default function WeatherForecast(props) {
                   <WeatherDayForecast data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
